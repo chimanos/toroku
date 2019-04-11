596bc8abe1dcd3233cd481de7247781ec15544ec
fberrez <florent.berrez@gmail.com>
2019-04-11 22:45:03 +0200

    fix merge conflicts

--- src/app/@core/tools/jsonParser.tools.ts
+++ src/app/@core/tools/jsonParser.tools.ts
@@ -5,73 +5,71 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class JsonParser {
 
-    constructor() {
-    }
+    constructor() {}
 
     getForm(service: ServiceData): Observable<string> {
-        let form = '<form [formGroup]="subscribeForm" id="subscribe" #form="ngForm">';
+        let form = '<form [formGroup]="subscribeForm" id="subscribeForm" #form="ngForm">';
         for (let i = 0; i < service.elements.length; i++) {
-            const element = service.elements[i];
-            switch (element.type.toLocaleLowerCase()) {
-                case 'image':
-                    form = `<ion-img style="width: 80px; height: 80px; margin-top: 20px; margin-left: auto; margin-right: auto;" src="${element.value[0]}"></ion-img>` + form;
-                    break;
-                case 'edit':
-                    form += `<ion-item>
-                  <ion-label>${element.value[0]}</ion-label>`;
-                    if (element.mandatory) {
-                        form += `<ion-input type="text" name="${element.value[0]}" placeholder="${element.value[0]}" value="${element.value[0]}" required></ion-input>`;
-                    } else {
-                        form += `<ion-input type="text" name="${element.value[0]}" placeholder="${element.value[0]}" value="${element.value[0]}"></ion-input>`;
-                    }
-                    form += `</ion-item>`;
-                    break;
-                case 'label':
-                    if (element.mandatory) {
-                        form += `<ion-label color="primary">${element.value[0]}</ion-label>`;
-                    } else {
-                        form += `<ion-label color="secondary">${element.value[0]}</ion-label>`;
-                    }
-                    break;
-                case 'switch':
-                    form += '<ion-item>';
-                    if (element.value[0]) {
-                        form += `<ion-checkbox slot="start" value="biff" checked></ion-checkbox>`;
-                    } else {
-                        form += `<ion-checkbox slot="start" value="biff"></ion-checkbox>`;
-                    }
-                    form += '</ion-item>';
-                    break;
-                case 'radiogroup':
-                    form += `<ion-list>
-                      <ion-radio-group>`;
-                    element.value.forEach(radio => {
-                        form += `<ion-item>
+          let element = service.elements[i]
+          switch(element.type.toLocaleLowerCase()) {
+            case "image":
+              form = `<ion-img [src]="${element.value[0]}"></ion-img>` + form
+            break;
+            case "edit":
+              form += `<ion-item>
+                  <ion-label>${element.value[0]}</ion-label>`
+              if (element.mandatory) {
+                form += `<ion-input type="text" name="${element.value[0]}" placeholder="${element.value[0]}" value="${element.value[0]}" required></ion-input>`
+              } else {
+                form += `<ion-input type="text" name="${element.value[0]}" placeholder="${element.value[0]}" value="${element.value[0]}"></ion-input>`
+              }
+                form += `</ion-item>`
+            break;
+            case "label":
+              if (element.mandatory) {
+                form += `<ion-label color="primary">${element.value[0]}</ion-label>`
+              } else {
+                form +=  `<ion-label color="secondary">${element.value[0]}</ion-label>`
+              }
+            break;
+            case "switch":
+              form += "<ion-item>"
+              if (element.value[0]) {
+                form += `<ion-checkbox slot="start" value="biff" checked></ion-checkbox>`
+              } else {
+                form += `<ion-checkbox slot="start" value="biff"></ion-checkbox>`
+              }
+              form += "</ion-item>"
+            break;
+            case "radiogroup":
+              form += `<ion-list>
+                      <ion-radio-group>`
+              element.value.forEach(radio => {
+                form += `<ion-item>
                           <ion-label>${radio}</ion-label>
                           <ion-radio value="${radio}"></ion-radio>
-                        </ion-item>`;
-                    });
+                        </ion-item>`
+              })
 
-                    form += `</ion-radio-group>
-              </ion-list>`;
-                    break;
-                case 'button':
-                    if (element.mandatory) {
-                        form += `<ion-button color="primary">${element.value[0]}</ion-button>`;
-                    } else {
-                        form += `<ion-button color="secondary">${element.value[0]}</ion-button>`;
-                    }
-                    break;
-                default:
-                    form += 'Type not implemented: ' + element.type;
-                    break;
+              form += `</ion-radio-group>
+              </ion-list>`
+            break;
+            case "button":
+            if (element.mandatory) {
+              form += `<ion-button color="primary">${element.value[0]}</ion-button>`
+            } else {
+              form += `<ion-button color="secondary">${element.value[0]}</ion-button>`
             }
+            break;
+            default:
+              form += "Type not implemented: " + element.type
+              break;
+          }
         }
-        form += '<ion-button id="subscribeButton" (click)="logForm()">Subscribe</ion-button>';
-        form += '</form>';
-        return new Observable<string>(emitter => {
-            emitter.next(form);
-            emitter.complete();
-        });
+        form += '<ion-button onclick="loadForm(event)">Subscribe</ion-button>'
+
+        //let test = function(e) { e.preventDefault(); console.log(\'here\')}; test(event)
+       form += '</form>'
+      return new Observable<string>(emitter => {emitter.next(form); emitter.complete(); });
     }
 }
