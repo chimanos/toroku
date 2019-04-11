a8309b05c1c1addc4114d208cf1c3deff8013ea5
fberrez <florent.berrez@gmail.com>
2019-04-11 19:41:32 +0200

    add json parser

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,12 +1,14 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ServiceService} from './service/service.service';
+import {JsonParser} from './tools/jsonParser.tools';
 import {HttpClientModule} from '@angular/common/http';
 import {ResultService} from './service/result.service';
 
 const SERVICES = [
     ServiceService,
-    ResultService
+    ResultService,
+    JsonParser
 ];
 
 @NgModule({
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,88 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, SecurityContext} from '@angular/core';
+import {ServiceService} from '../@core/service/service.service';
+import {JsonParser} from '../@core/tools/jsonParser.tools';
+import {Observable} from 'rxjs';
+import {Storage} from '@ionic/storage';
+import {flatMap} from 'rxjs/operators';
+import {BrowserModule, DomSanitizer, SafeHtml} from '@angular/platform-browser'
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    template: `
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>
+                    Tab Two
+                </ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content [innerHTML]="form">
+        </ion-content>
+    `,
+    styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+export class Tab2Page implements OnInit {
+    form$: Observable<string>;
+    observer: any;
+    form: SafeHtml
+    CURRENT_SERVICE_KEY = 'currentService';
+
+    constructor(private serviceService: ServiceService, private jsonParser: JsonParser, private storage: Storage, private sanitizer: DomSanitizer) {
+      this.form = undefined
+    }
+
+    ngOnInit(): void {
+        this.form$ = this.serviceService.getServices().pipe(
+            flatMap(services => {
+                return this.getCurrentService().pipe(
+                    flatMap(currentServiceName => {
+                        return this.jsonParser.getForm(services.find(service => {
+                            return service.title === currentServiceName;
+                        }));
+                    })
+                );
+            })
+        );
+
+        this.observer = this.form$.subscribe(
+        (result) => {
+          this.form = this.sanitizer.bypassSecurityTrustHtml(result);
+        },
+        (error) => {
+          console.error("Error", error);
+        },
+        () => {
+          console.log("Subscription complete");
+        }
+      )
+    }
+
+    ionViewDidLeave() {
+      this.observer.unsubscribe(
+        (result) => {
+          console.log("unsubscribe done");
+        },
+        (error) => {
+          console.error("Error", error);
+        },
+        () => {
+          console.log("unsubscription complete");
+        }
+      )
+    }
+
+    getCurrentService(): Observable<string> {
+        return new Observable<string>(emitter => {
+            this.storage.get(this.CURRENT_SERVICE_KEY).then((serviceName) => {
+                emitter.next(serviceName);
+                emitter.complete();
+            });
+        });
+    }
+
+    logForm(): void {
+      console.log("test")
+    }
+}
